a5baaaaf861b14d15af3830316fbe69489afd422
gustav-evensson <evenssongustav@gmail.com>
2023-01-11 08:59:31 +0100

    examples

---
+++ examples/vue-project/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import noticeComponent from './components/noticeComponent.vue';
+</script>
+
+<template>
+  <div class="home">
+    <notice-component ref="noticeContainer" class="dark bottom-center"/>
+    <button @click="$refs.noticeContainer.createNotice('test from parent', 'alert', 'persisted')">Create Notice</button>
+  </div>
+</template>
+
+<style scoped>
+  .home{
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  button{
+    background-color: white;
+    color: black;
+    border: none;
+    font-size: 20px;
+    border-radius: 12px;
+    padding: 10px 30px;
+  }
+</style>
